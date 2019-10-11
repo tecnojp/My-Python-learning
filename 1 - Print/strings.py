97ba3a84b1a96c6d86cc20f89c3328258a4bd5a8
João Paulo Oliveira <jpaulo.oliveira@brainfarma.ind.br>
2019-10-11 13:02:38 -0300

    Playing with strings

---
+++ 1 - Print/strings.py
@@ -0,0 +1,16 @@
+# Playing with strings and operators
+first_name = 'JP'
+last_name = 'Oliveira'
+print (first_name + last_name)
+print (first_name + ' ' + last_name)
+
+# Playing with strings and functions
+sentence = "The Dog is named Sammy"
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.capitalize())
+print(sentence.count('a'))
+
+first_name = input('What is yuour fist name? ')
+last_name = input('What is your last name?' )
+print ('Hello ' + first_name.capitalize() + ' ' + last_name.capitalize())
