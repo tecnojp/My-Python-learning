fe81ab29a662a53f96cb1f426e3bee1fccce6121
João Paulo Oliveira <jpaulo.oliveira@brainfarma.ind.br>
2019-10-11 15:19:13 -0300

    Playing with Strings and Variables

--- 1 - Print/strings.py
+++ 1 - Print/strings.py
@@ -14,3 +14,10 @@ print(sentence.count('a'))
 first_name = input('What is yuour fist name? ')
 last_name = input('What is your last name?' )
 print ('Hello ' + first_name.capitalize() + ' ' + last_name.capitalize())
+output = 'Hello, {} {}'.format(first_name,last_name)
+print(output)
+output = 'Hello, {0} {1}'.format(first_name,last_name)
+print(output)
+# Only available in Python 3
+output = f'Hello, {first_name} {last_name}'
+print(output)
